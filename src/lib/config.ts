dfa2867ffb42be60f47f54e2aba3654716a57d9b
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2019-04-01 00:57:41 +0200

    Last updated info

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -1,3 +1,15 @@
+const lastUpdated = new Date();
+const lastUpdatedOptions = {
+    timeZone: 'UTC',
+    hour12: false,
+    year: 'numeric',
+    month: 'long',
+    day: '2-digit',
+    hour: '2-digit',
+    minute: '2-digit',
+    second: '2-digit',
+};
+const lastUpdatedString = lastUpdated.toLocaleString('en-US', lastUpdatedOptions);
 
 const config = {
     title: 'Nicolas Hollmann | Github.io',
@@ -5,6 +17,7 @@ const config = {
     author: 'Nicolas Hollmann',
     description: 'Nicolas Hollmann\'s GitHub repositories.',
     keywords: 'Nicolas Hollmann, GitHub, Repsoitories',
+    lastUpdated: lastUpdatedString,
 };
 
 export default config;
