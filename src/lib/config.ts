27b8534464bc550bfe13e36f326fa7690f13baa5
Nicolas Hollmann <43784252+NHollmann@users.noreply.github.com>
2019-04-01 00:27:25 +0200

    First usefule website

--- src/lib/config.ts
+++ src/lib/config.ts
@@ -3,6 +3,8 @@ const config = {
     title: 'Nicolas Hollmann | Github.io',
     user: 'nhollmann',
     author: 'Nicolas Hollmann',
+    description: 'Nicolas Hollmann\'s GitHub repositories.',
+    keywords: 'Nicolas Hollmann, GitHub, Repsoitories',
 };
 
 export default config;
